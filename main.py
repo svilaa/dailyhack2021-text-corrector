6be4ef72c958fc0d591e1e9ac0d3909d44f69028
svila <svilaalmenara@gmail.com>
2021-11-19 17:16:55 +0100

    Improvements.

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
 # -*- coding: utf-8 -*-
-
-import re
-from thefuzz import fuzz
-from nltk.tokenize import WordPunctTokenizer
-
-def correct_text():
-    dictionary =[w.strip() for w in open("dictionary.txt", encoding="utf-8")]
-    avoid_tokens = [",", ".", ":", ";"]
-    with open("original.txt", encoding="utf-8") as input_file:
-        with open("fixed4.txt", 'w', encoding="utf-8") as output_file:
-            for value in input_file.readlines():
-                words = WordPunctTokenizer().tokenize(re.sub(r'\S?/\S?', "", re.sub(r'\.\w', ".", value)))
-                for word in words:
-                    if word not in avoid_tokens:
-                        output_file.write(" ")
-                        word = max([[dict_word, fuzz.ratio(word, dict_word)] for dict_word in dictionary], key=lambda x:x[1])[0]
-                    output_file.write(word)
-                output_file.write("\n")
-
-if __name__=="__main__":
-    correct_text()
-    
+import re as r;from thefuzz import fuzz as f;from nltk.tokenize import WordPunctTokenizer as t
+d = [w.strip() for w in open("dictionary.txt", encoding="utf-8")]
+with open("original.txt", encoding="utf-8") as i:
+    with open("corrected.txt", 'w', encoding="utf-8") as o:
+        for v in i.readlines():
+            for w in t().tokenize(r.sub(r'\S?/\S?', "", r.sub(r'\.\w', ".", v))):
+                if w not in [",", ".", ":", ";"]: w = " " + max([[c, f.ratio(w, c)] for c in d], key=lambda x:x[1])[0]
+                o.write(w)
+            o.write("\n")
